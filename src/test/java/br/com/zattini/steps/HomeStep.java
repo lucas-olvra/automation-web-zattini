607296df3d18aadec6386697035e9fdb4dc6006f
lucas-olvra <lucas2000544@gmail.com>
2024-05-24 22:01:08 -0300

    :chore: construindo o projeto e criando os steps do gherkin com cucumber

---
+++ src/test/java/br/com/zattini/steps/HomeStep.java
@@ -0,0 +1,11 @@
+package br.com.zattini.steps;
+
+import io.cucumber.java.pt.Entao;
+
+public class HomeStep {
+
+    @Entao("devo visualizar o nome {string}")
+    public void visualizarONome(String nome){
+        
+    }
+}
